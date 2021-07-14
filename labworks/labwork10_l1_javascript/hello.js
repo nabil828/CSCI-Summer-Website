49f50f45b6ab4e1d3f4a7ab58a9132e03b893b02
Nabil M. Al-Rousan <rousani@gmail.com>
2021-07-14 15:35:50 -0700

    jQuery example
    to be continued

--- labworks/labwork10_l1_javascript/hello.js
+++ labworks/labwork10_l1_javascript/hello.js
@@ -1,20 +1,13 @@
-multiply_by5  = function(number_to_be_multiplied, name) {
-  // y = x * x ;
-  // z = "The square result is ";
-  // w = z + y;
-  // alert('Hello'  + ' ' + 'world!');
-
-  // This is comment
-  // x is an object
-  // student1 = {weight:70, age:21, name:"Arshdeep"};
-  // student2 = {weight:60, age:19, name:"Kasheesh"};
-  // alert("name of student1: " + student1.name);
-  // alert("name of student2: " + student2.name);
-
-  // x = 1;
-  // y = 2;
-  // alert( x - y);
-
-  result =  5 * number_to_be_multiplied ;
-  alert("My name is " + name + " and the result is " + result)
+multiply_by5  = function() {
+  result =  5 * 3 ;
+  alert("My name is " + 'Nabil' + " and the result is " + result)
+  alert('hi');
 }
+
+
+setup = function (){
+  jQuery('p').click(multiply_by5);
+  // all your events are going to be listed here
+}
+
+jQuery(document).ready(setup);
