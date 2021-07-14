9d95a15a288a89920c54c444afbcf058dea43db1
Nabil M. Al-Rousan <rousani@gmail.com>
2021-07-14 15:10:39 -0700

    lab 10

--- hello.js
+++ hello.js
@@ -1,4 +1,20 @@
-say_hello = function(x) {
-  y = x * x ;
-  alert( y);
+multiply_by5  = function(number_to_be_multiplied, name) {
+  // y = x * x ;
+  // z = "The square result is ";
+  // w = z + y;
+  // alert('Hello'  + ' ' + 'world!');
+
+  // This is comment
+  // x is an object
+  // student1 = {weight:70, age:21, name:"Arshdeep"};
+  // student2 = {weight:60, age:19, name:"Kasheesh"};
+  // alert("name of student1: " + student1.name);
+  // alert("name of student2: " + student2.name);
+
+  // x = 1;
+  // y = 2;
+  // alert( x - y);
+
+  result =  5 * number_to_be_multiplied ;
+  alert("My name is " + name + " and the result is " + result)
 }
