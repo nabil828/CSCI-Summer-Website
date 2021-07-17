9d416038395b1fd0829af6b300172c3cba44e398
Nabil M. Al-Rousan <rousani@gmail.com>
2021-07-17 14:58:55 -0700

    append_it Vs. change_it

--- labworks/labwork110_l3_selectors/script.js
+++ labworks/labwork110_l3_selectors/script.js
@@ -1,9 +1,13 @@
+append_it = function(){
+  old_value =   jQuery('ul>li').html();
+  jQuery('ul>li').html(old_value  + "new value")
+}
 change_it = function() {
     jQuery('ul>li').html('new value')
     // alert('change_it has been called')
 }
 setup = function() {
-    jQuery('ul>li').click(change_it)
+    jQuery('ul>li').click(append_it)
 }
 
 jQuery(document).ready(setup)
