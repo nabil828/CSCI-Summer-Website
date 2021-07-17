569c2fc58acff3b2482886d03ba2f1ec19767e12
Nabil M. Al-Rousan <rousani@gmail.com>
2021-07-17 14:44:00 -0700

    ID selectors in JS

--- labworks/labwork110_l3_selectors/script.js
+++ labworks/labwork110_l3_selectors/script.js
@@ -1,9 +1,9 @@
 change_it = function() {
-    jQuery('.blueish_color').html('some text <a href="">alink</a>')
-    // alert('change_it has been called')
+    jQuery('#changing').html('some text <a href="">alink</a>')
+    alert('change_it has been called')
 }
 setup = function() {
-    jQuery('.blueish_color').click(change_it)
+    jQuery('#changing').click(change_it)
 }
 
 jQuery(document).ready(setup)
